2ada3cf18f0a078c5abd241c25d6de25a4fa2a99
EDDY WANG <ewang@uaterloo.ca>
2014-03-09 19:02:04 -0400

    added map and coordinates

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,14 +1,14 @@
-﻿
-var app = angular.module('app', []); 
+﻿var app = angular.module('app', []);
 
 var url = 'http://localhost:1303/QXpoService.svc/';
 
 
-app.factory('personFactory', function ($http) { 
-            return { 
-                getWaitTime: function () { 
-                    return $http.get(url+'GetWaitTimeForAllRides'); 
-                }/* , 
+app.factory('personFactory', function ($http) {
+    return {
+        getWaitTime: function () {
+            return $http.get(url + 'GetWaitTimeForAllRides');
+        }
+        /* , 
                 addPerson: function (person) { 
                     return $http.post(url, person); 
                 }, 
@@ -18,25 +18,25 @@ app.factory('personFactory', function ($http) {
                 updatePerson: function (person) { 
                     return $http.put(url + person.Id, person); 
                 }  */
-            }; 
-        }); 
-         
-        app.factory('notificationFactory', function () { 
-  
-            return { 
-                success: function () { 
-                    toastr.success("Success"); 
-                }, 
-                error: function (text) { 
-                    toastr.error(text, "Error!"); 
-                } 
-            }; 
-        }); 
-  
- app.controller('IndexCtrl', function ($scope, personFactory, notificationFactory) { 
-	
-	//makeCorsRequest();
-/* 	var xmlhttp;
+    };
+});
+
+app.factory('notificationFactory', function () {
+
+    return {
+        success: function () {
+            toastr.success("Success");
+        },
+        error: function (text) {
+            toastr.error(text, "Error!");
+        }
+    };
+});
+
+app.controller('IndexCtrl', function ($scope, personFactory, notificationFactory) {
+
+    //makeCorsRequest();
+    /* 	var xmlhttp;
 	if (window.XMLHttpRequest)
 	  {// code for IE7+, Firefox, Chrome, Opera, Safari
 	  xmlhttp=new XMLHttpRequest();
@@ -54,70 +54,173 @@ app.factory('personFactory', function ($http) {
 	}
 	xmlhttp.open("GET",'http://localhost:1303/QXpoService.svc/GetWaitTimeForAllRides',true);
 	xmlhttp.send(); */
-	
-	
-	/* var successCallback = function (data, status, headers, config) { 
+
+
+    /* var successCallback = function (data, status, headers, config) { 
 		notificationFactory.success(); 
 
 		return personFactory.getWaitTime().success(getPeopleSuccessCallback).error(errorCallback); 
-	}; */ 
-	var getPeopleSuccessCallback = function (data, status) { 
-				var x2js = new X2JS();
-				
-                var tobj = x2js.xml_str2json(data).string.__text; 
-				$scope.waitTimes = x2js.xml_str2json(tobj);
-    }; 
+	}; */
+
+    $scope.waitTimes = [];
+    var PADDING = 0;
+    var stage = new Kinetic.Stage({
+        container: 'container',
+        width: 800,
+        height: 800
+    });
+
+    var layer = new Kinetic.Layer();
+	var imageObj = new Image();
+    imageObj.onload = function() {
+        var map = new Kinetic.Image({
+          x: 0,
+          y: 0,
+          image: imageObj,
+          width: 800,
+          height: 800
+        });
+		layer.add(map);
+	};
+	imageObj.src = 'img/parkMap.jpg';
+    var getPeopleSuccessCallback = function (data, status) {
+        var x2js = new X2JS();
+
+        var tobj = x2js.xml_str2json(data).string.__text;
+        $scope.waitTimes = x2js.xml_str2json(tobj).data.entry;
+        $scope.waitTimes.forEach(function (node) {
+
+            var coordX = node.coordinate.split(',')[0];
+            var coordY = node.coordinate.split(',')[1]; //horrible
+            var stageX = coordX * stage.getWidth() / 100 + PADDING;
+            var stageY = coordY * stage.getHeight() / 100 + PADDING;
+            var group = new Kinetic.Group({
+                draggable: true
+            });
+	//CIRCLE START		
+            var circle = new Kinetic.Circle({
+                x: stageX,
+                y: stageY,
+                radius: 10,
+                fill: 'white',
+                stroke: 'black',
+                strokeWidth: 1
+            });
+			
+	//labelLeft START		
+            var labelLeft = new Kinetic.Label({
+                x: stageX,
+                y: stageY,
+                opacity: 0.9
+            });
 	
-	var errorCallback = function (data, status, headers, config) { 
-                notificationFactory.error(data.ExceptionMessage); 
+            var labelDir = "";
+            if ((stage.getWidth() - stageX) < 100) {
+                labelDir = "right";
+            } else {
+                labelDir = "left";
+            }
+            labelLeft.add(new Kinetic.Tag({
+                fill: 'white',
+                pointerDirection: labelDir,
+                pointerWidth: 10,
+                pointerHeight: 24,
+                lineJoin: 'round'
+            }));
+
+            labelLeft.add(new Kinetic.Text({
+                text: node.wait_time + " mins",
+                fontFamily: 'Calibri',
+                fontSize: 14,
+                padding: 5,
+                fill: 'black'
+            }));
+	//IDLABEL START
+            var idLabel = new Kinetic.Label({
+                x: stageX - 8,
+                y: stageY - 8,
+                opacity: 1
+            });
+            idLabel.add(new Kinetic.Text({
+                text: node.ride_id,
+                fontFamily: 'Calibri',
+                fontSize: 16,
+                padding: 0,
+                fill: 'black'
+            }));
+	//GROUPS
+            group.add(circle);
+            group.add(labelLeft);
+            group.add(idLabel);
+            group.on('mouseover', function () {
+                document.body.style.cursor = 'pointer';
+            });
+            group.on('mouseout', function () {
+                document.body.style.cursor = 'default';
+            });
+			
+	//FINALIZING
+            layer.add(group);
+            stage.add(layer);
+        });
     };
-	personFactory.getWaitTime().success(getPeopleSuccessCallback).error(errorCallback); 
- });
- 
- 
- function makeCorsRequest() {
-  var url = 'http://localhost:1303/QXpoService.svc/GetWaitTimeForAllRides';
 
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
+    var errorCallback = function (data, status, headers, config) {
+        notificationFactory.error(data.ExceptionMessage);
+    };
+    personFactory.getWaitTime().success(getPeopleSuccessCallback).error(errorCallback);
 
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    //var title = getTitle(text);
-    alert('Response from CORS request to ' + url );
-  };
 
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
 
-  xhr.send();
+
+});
+
+
+
+
+function makeCorsRequest() {
+    var url = 'http://localhost:1303/QXpoService.svc/GetWaitTimeForAllRides';
+
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    // Response handlers.
+    xhr.onload = function () {
+        var text = xhr.responseText;
+        //var title = getTitle(text);
+        alert('Response from CORS request to ' + url);
+    };
+
+    xhr.onerror = function () {
+        alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
 }
 
 function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
 
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
 
-  } else if (typeof XDomainRequest != "undefined") {
+    } else if (typeof XDomainRequest != "undefined") {
 
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
 
-  } else {
+    } else {
 
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
 
-  }
-  return xhr;
+    }
+    return xhr;
 }
